0fd64cc6a87efd15e982aeae6b147bd90e34b9f2
Sergei Pleshanov <2357qwr@gmail.com>
2022-11-28 00:24:34 +0300

    refactor: change position on the btn-edit and fix btn-like

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,31 @@
 const popupElem = document.querySelector('.popup');
-const editButton = document.querySelector('.button.profile__btn-edit');
-const editProfileForm = document.querySelector('.form');
+const buttonPopupClose = popupElem.querySelector('.button.popup__btn-close');
+const buttonEditProfile = document.querySelector('.button.profile__btn-edit');
+const formEditProfile = document.querySelector('.form');
+const formInputName = formEditProfile.querySelector('input[name=name]');
+const formInputJob = formEditProfile.querySelector('input[name=job]');
 const profile = document.querySelector('.profile__info');
+const profileName = profile.querySelector('.profile__title');
+const profileJob = profile.querySelector('.profile__subtitle');
 
 const onOpen = (popup) => popup.classList.add('popup_active');
 const onClose = (popup) => popup.classList.remove('popup_active');
 
-const onSubmit = (form) => {
-  profile.children[0].textContent = form[0].value;
-  profile.children[1].textContent = form[1].value;
+const onSubmit = (evt) => {
+  evt.preventDefault();
+  profileName.textContent = formInputName.value;
+  profileJob.textContent = formInputJob.value;
   onClose(popupElem);
 };
 
-editButton.addEventListener('click', () => {
-  editProfileForm[0].value = profile.children[0].textContent;
-  editProfileForm[1].value = profile.children[1].textContent;
+buttonEditProfile.addEventListener('click', () => {
+  formInputName.value = profileName.textContent;
+  formInputJob.value = profileJob.textContent;
   onOpen(popupElem);
 });
 
-popupElem.addEventListener('click', (event) => {
-  const isClose = event.target.classList.contains('popup__btn-close');
-  const isSave = event.target.classList.contains('popup__btn-save');
-
-  if (isClose) onClose(popupElem);
-
-  if (isSave) {
-    event.preventDefault();
-    onSubmit(editProfileForm);
-  }
+buttonPopupClose.addEventListener('click', () => {
+  onClose(popupElem)
 });
+
+popupElem.addEventListener('submit', onSubmit);
