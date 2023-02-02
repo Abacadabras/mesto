1c73d502db9183518c8c6980aeeda47a29c66a1a
Sergei Pleshanov <2357qwr@gmail.com>
2023-02-02 15:12:38 +0300

    refactor: minor edit index.js and index.html

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import { dataCards, validationConfig, cardConfig } from '../utils/constants.js';
-import Card from '../components/Card.js';
-import FormValidator from '../components/FormValidator.js'
-import Section from '../components/Section.js';
-import PopupWithImage from '../components/PopupWithImage.js';
-import PopupWithForm from '../components/PopupWithForm.js';
-import UserInfo from '../components/UserInfo.js';
+import { dataCards, validationConfig, cardConfig } from '../scripts/utils/constants.js';
+import Card from '../scripts/components/Card.js';
+import FormValidator from '../scripts/components/FormValidator.js'
+import Section from '../scripts/components/Section.js';
+import PopupWithImage from '../scripts/components/PopupWithImage.js';
+import PopupWithForm from '../scripts/components/PopupWithForm.js';
+import UserInfo from '../scripts/components/UserInfo.js';
 
 
 //Popups profile
@@ -24,31 +24,11 @@ const profileFormValidator = new FormValidator(validationConfig, formProfile);
 const addCardFormValidator = new FormValidator(validationConfig, formAddCard);
 
 const popupImg = new PopupWithImage(popupImgSelector);
-
-const placesList = new Section({
-  items: dataCards,
-  renderer: (place) => {
-    const card = new Card(cardConfig, place, handleImgCard);
-    const cardElement = card.generateCard();
-    placesList.addItem(cardElement);
-  }
-}, placesSelector);
-
 const user = new UserInfo(userNameSelector, userDescriptionSelector);
 
 const handleImgCard = (name, link) => {
   popupImg.open(name, link);
 };
-
-buttonEditProfile.addEventListener('mousedown', () => {
-  popupProfile.setInputValues(user.getUserInfo());
-  profileFormValidator.resetErrors();
-  popupProfile.open();
-});
-
-buttonAddPlace.addEventListener('mousedown', () => {
-  popupAddCard.open();
-})
 
 const handleSubmitProfile = (evt) => {
   evt.preventDefault();
@@ -65,9 +45,27 @@ const handleSubmitCard = (evt) => {
   placesList.addItem(cardElement);
   addCardFormValidator.resetSubmit();
 };
+const placesList = new Section({
+  items: dataCards,
+  renderer: (place) => {
+    const card = new Card(cardConfig, place, handleImgCard);
+    const cardElement = card.generateCard();
+    placesList.addItem(cardElement);
+  }
+}, placesSelector);
 
 const popupProfile = new PopupWithForm(popupProfileSelector, handleSubmitProfile);
 const popupAddCard = new PopupWithForm(popupAddCardSelector, handleSubmitCard);
+
+buttonEditProfile.addEventListener('mousedown', () => {
+  popupProfile.setInputValues(user.getUserInfo());
+  profileFormValidator.resetErrors();
+  popupProfile.open();
+});
+
+buttonAddPlace.addEventListener('mousedown', () => {
+  popupAddCard.open();
+})
 
 placesList.renderItems();
 popupImg.setEventListeners();
@@ -75,5 +73,3 @@ popupProfile.setEventListeners();
 popupAddCard.setEventListeners();
 profileFormValidator.enableValidation();
 addCardFormValidator.enableValidation();
-
-
